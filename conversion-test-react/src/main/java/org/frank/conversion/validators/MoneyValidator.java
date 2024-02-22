d036fb73c546d2405f160903cba07a43d4b8942d
fengandzhy <fengandzhy@gmail.com>
2024-02-22 17:52:20 +1300

    validator

---
+++ conversion-test-react/src/main/java/org/frank/conversion/validators/MoneyValidator.java
@@ -0,0 +1,20 @@
+package org.frank.conversion.validators;
+
+
+
+import org.frank.conversion.annotations.Money;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MoneyValidator implements ConstraintValidator<Money, String> {
+    
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        Pattern pattern = Pattern.compile("^(([1-9]{1}\\d*)|([0]{1}))(\\.(\\d){0,2})?$");
+        Matcher m = pattern.matcher(value);
+        return m.matches();
+    }
+}
