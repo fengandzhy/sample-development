46d1fb16c55ffb17b151f0575605c9aeda216177
fengandzhy <fengandzhy@gmail.com>
2024-02-22 16:50:03 +1300

    tests

---
+++ conversion-test/src/test/java/org/frank/conversion/enums/TensEnumTest.java
@@ -0,0 +1,17 @@
+package org.frank.conversion.enums;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class TensEnumTest {
+    
+    @Test
+    public void testGetWord(){
+        String word = TensEnum.getWord(3);
+        assertEquals("THIRTY", word);
+
+        word = TensEnum.getWord(9);
+        assertEquals("NINETY", word);
+    }
+}
