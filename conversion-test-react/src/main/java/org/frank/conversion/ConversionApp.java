d390a29d0312136c373a978d06e20868cacb5ece
fengandzhy <fengandzhy@gmail.com>
2024-02-22 17:35:40 +1300

    ConversionApp.java

---
+++ conversion-test-react/src/main/java/org/frank/conversion/ConversionApp.java
@@ -0,0 +1,12 @@
+package org.frank.conversion;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversionApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConversionApp.class, args);
+    }
+}
