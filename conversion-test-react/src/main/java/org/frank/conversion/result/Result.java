c907edd92241c85309612f7215f2ff39c9ec0463
fengandzhy <fengandzhy@gmail.com>
2024-02-22 17:52:49 +1300

    result

---
+++ conversion-test-react/src/main/java/org/frank/conversion/result/Result.java
@@ -0,0 +1,44 @@
+package org.frank.conversion.result;
+
+
+import org.frank.conversion.enums.ResultStatus;
+
+@SuppressWarnings("unused")
+public class Result<T> {
+
+    
+    private final Integer code;
+    
+    private final String message;
+    
+    private final T data;
+
+    private Result(ResultStatus resultStatus, T data){
+        this.code = resultStatus.getCode();
+        this.message = resultStatus.getMessage();
+        this.data = data;
+    }   
+
+    public static <T> Result<T> success(T data){
+        return new Result<>(ResultStatus.SUCCESS,data);
+    }   
+
+    public static <T> Result<T> failure(ResultStatus resultStatus,T data){
+        if(resultStatus == null){
+            return new Result<>(ResultStatus.INTERNAL_SERVER_ERROR,null);
+        }
+        return new Result<>(resultStatus,data);
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public T getData() {
+        return data;
+    }
+}
